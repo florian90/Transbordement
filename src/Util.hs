52b13e837c402d05c4b3a9d2af52789fd444ea15
florian90 <florian.staine@utbm.fr>
2016-05-16 11:24:00 +0200

    new class : Indexable   Must write the function 'idx' that returns the index of the element

--- src/Util.hs
+++ src/Util.hs
@@ -4,7 +4,28 @@ type ID = Int
 type ValType = Double
 type Path = (ID, ID, Int)
 
+class Indexable a where
+    idx :: a -> ID
+
+{-
+    Get a word in a String
+      If the index is -1 return the last word,
+      otherwise, returns the word of index idx
+-}
 (#) :: (Read a) => String -> Int -> a
 (#) str idx
     | idx == -1 = read . last . words $ str
     | otherwise = read $ (words str)!!idx
+
+{-
+    Add an element at the right index of a list so that :
+      idx (l!!x) == x
+    The list must already contains enough elements
+      to add the new element at the right position
+-}
+(§) :: (Indexable a) => [a] -> a -> [a]
+(§) list el = replace (idx el) el list
+
+replace n newVal (x:xs)
+    | n == 0 = newVal:xs
+    | otherwise = x:replace (n-1) newVal xs
